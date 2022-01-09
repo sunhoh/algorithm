2d3a4a91f394f79bd756c25962ac7dc6df11f46a
이선호 <dltjsgho94@gmail.com>
2022-01-09 15:29:39 +0900

    Add: K번쨰수

--- 로또의 최고 순위와 최저 순위.js
+++ 로또의 최고 순위와 최저 순위.js
@@ -1,5 +1,24 @@
+const lottos = [44, 1, 0, 0, 31, 25]
+const win_nums = [31, 10, 45, 1, 6, 19]
+
 function solution(lottos, win_nums) {
-    var answer = [];
-    return answer;
+  var answer = [];
+  let count = 0;
+  const rank = [6, 6, 5, 4, 3, 2, 1];
+  
+  const zeroCount = lottos.filter( e => e === 0 ).length;
+  
+  lottos.forEach( e => {
+    const index = win_nums.indexOf(e);
+    console.log(index)
+    
+      if( index !== -1 ) {
+          count++;
+          win_nums.splice(index, 1);
+        }
+    });
+    
+    
+    return [rank[count + zeroCount], rank[count]];
 }
-solution([[44, 1, 0, 0, 31, 25],[31, 10, 45, 1, 6, 19]])
+solution(lottos, win_nums)
