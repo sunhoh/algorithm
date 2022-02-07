09252e2926a151ac76c204635dbaccb2a5cabab3
이선호 <dltjsgho94@gmail.com>
2022-02-07 18:48:51 +0900

    Add: 두개 뽑아서 더하기

---
+++ programmers/두 개 뽑아서 더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+    const result = []
+    
+    for(let i = 0; i < numbers.length - 1; i++){
+        for(let j = i+1; j < numbers.length; j++){
+            const sum = numbers[i] + numbers[j]
+            if(result.indexOf(sum) < 0){
+                result.push(sum)
+            }
+        }    
+    }
+    
+    result.sort((a,b)=>a-b)
+    return result
+}
