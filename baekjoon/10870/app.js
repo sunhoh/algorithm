ec8887bde37d93a52bd0d0ecd8e046a2ac8e6663
sunhoh <dltjsgho94@gmail.com>
2023-12-16 12:06:09 +0900

    beakjoon: 10870

---
+++ baekjoon/10870/app.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt'
+const input = fs.readFileSync(filePath).toString().trim()
+
+function fibonacci(n){
+  if(n === 0) return 0
+  else if(n === 1) return 1
+  else return fibonacci(n-1) + fibonacci(n-2)
+}
+
+console.log(fibonacci(input))
