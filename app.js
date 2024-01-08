5823f070831ab2e426a23dea8cdbc1c44a96f21e
sunhoh <dltjsgho94@gmail.com>
2024-01-08 18:31:06 +0900

    baekjoon: 9093

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
 const path = process.platform === "linux" ? "/dev/stdin" : "input.txt"; 
 const input = require("fs").readFileSync(path).toString().trim().split("\n");
-
 const [n, ...testcases] = input;
- const arr = testcases.map((item) => item.split(' ').map(Number));
 
-let answer = '';
-
-for (let i = 0; i < arr.length; i += 2) {
-  let count = 0;
-  const priorities = arr[i + 1];
-  let location = arr[i][1];
-
-  while (true) {
-    const max = Math.max(...priorities);
-    const number = priorities.shift();
-    if (number === max) {
-      count++;
-      if (location === 0) {
-        answer += count + '\n';
-        break;
-      }
-    } else {
-      priorities.push(number);
-    }
-
-    if (location === 0) {
-      location = priorities.length - 1;
-    } else {
-      location--;
-    }
-  }
+for (let i = 0; i < n; i++) {
+  let words = testcases[i].split(" ");
+  let answer = words.map((word) => word.split("").reverse().join("")).join(" ");
+  console.log(answer);
 }
-
-console.log(answer.trim());
