ce14ec9834b0dc0765448e25a1e7c542cc943c56
sunhoh <dltjsgho94@gmail.com>
2024-01-21 23:33:53 +0900

    baekjoon

--- baekjoon/9093/app.js
+++ baekjoon/9093/app.js
@@ -7,3 +7,14 @@ for (let i = 0; i < n; i++) {
   let answer = words.map((word) => word.split("").reverse().join("")).join(" ");
   console.log(answer);
 }
+
+
+// const result = testcases.map((char) => {
+//   const offset = char.split(' ');
+//   let answer = '';
+//   for (let key of offset) {
+//     answer += key.split('').reverse().join('') + ' ';
+//   }
+//   return answer.trim();
+// });
+// console.log(result.join('\n'));
