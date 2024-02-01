56bc0d50a579276332a948a6c6307209d6508818
sunhoh <dltjsgho94@gmail.com>
2024-02-01 17:39:58 +0900

    baekjoon: 1978

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = process.platform === 'linux' ? '/dev/stdin' : 'input.txt'
+const input = require('fs').readFileSync(path).toString().trim().split('\n')
+
+const [n, testcase] = input
+
+const isPrime = (n) => {
+  if (n == 1) {
+    return false;
+  }
+  
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(testcase.split(" ").filter(v => isPrime(v)).length);
