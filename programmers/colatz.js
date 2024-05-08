29014731af5150e56cbfcc1b4d1fd377a95b1771
sunhoh <dltjsgho94@gmail.com>
2024-05-08 09:26:03 +0900

    add: colatz

---
+++ programmers/colatz.js
@@ -0,0 +1,14 @@
+function solution(num) {
+    let answer = 0;
+    for (i=0;i<500;i++){
+        if(num != 1){
+            num = num%2 == 0? num / 2 : num * 3 + 1;
+        }else{
+            return answer = i;
+        }
+    }
+    console.log(answer)
+    return answer = -1;
+}
+
+solution(6)
---
+++ programmers/poketmon.js
@@ -0,0 +1,15 @@
+function solution(nums) {
+    const limit = nums.length / 2
+
+    const obj = {}
+    for(i=0;i<nums.length;i++){
+      const cur = nums[i]
+      obj[cur] = !obj[cur] ? 1 : obj[cur] + 1 
+    }
+    
+    return Object.keys(obj).length > limit ? limit : Object.keys(obj).length
+  }
+  
+  solution([3,2,1,3])
+
+  
