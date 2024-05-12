b56d5035866490800af586bf41632cb63e6cec0b
sunhoh <dltjsgho94@gmail.com>
2024-05-12 18:11:51 +0900

    add: programmers py

---
+++ programmers/adding as many as an array.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for i in range(len(arr)):
+        for _ in range(arr[i]):
+            answer.append(arr[i])
+    return answer
+
+    # return [x for x in arr for _ in range(x)]
+
+print(solution([5,1,4]))
