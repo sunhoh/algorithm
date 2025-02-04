4f32fde5c495bcbb0c21748f808034cf7890a08a
Sunho Lee <88010585+sunhoh@users.noreply.github.com>
2025-02-04 17:05:12 +0900

    Delete 0009-palindrome-number directory

--- 0009-palindrome-number/0009-palindrome-number.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-    if (x < 0) return false;
-    let str = x.toString();
-    return str === str.split("").reverse().join("");
-};
