874b72444901b989bf8b7d688dd3fbbcc00fa473
Sunho Lee <88010585+sunhoh@users.noreply.github.com>
2025-02-18 09:50:33 +0900

    Time: 1 ms (63.77%), Space: 52.8 MB (29.44%) - LeetHub

--- leetcode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ leetcode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
+  let index = 1;
+  for (let i = 1; i < nums.length; i++) {
     if (nums[i] !== nums[i - 1]) {
       nums[index] = nums[i];
       index++;
