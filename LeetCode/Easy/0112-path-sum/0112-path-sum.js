ebc5b39fdd988a5a09b4faa4d0611cf24b709eac
Sunho Lee <88010585+sunhoh@users.noreply.github.com>
2025-09-05 13:20:12 +0900

    Time: 1 ms (53.1%), Space: 58.5 MB (47.22%) - LeetHub

--- LeetCode/Easy/0112-path-sum/0112-path-sum.js
+++ LeetCode/Easy/0112-path-sum/0112-path-sum.js
@@ -11,14 +11,24 @@
  * @param {number} targetSum
  * @return {boolean}
  */
-var hasPathSum = function (root, targetSum = 22) {
+ // BFS Bottom-Up
+// var hasPathSum = function (root, targetSum = 22) {
+//   if (!root) return false;
+//   const q = [[root, root.val]];
+//   while (q.length) {
+//     const [node, sum] = q.shift();
+//     if (!node.left && !node.right && sum === targetSum) return true;
+//     if (node.left) q.push([node.left, sum + node.left.val]);
+//     if (node.right) q.push([node.right, sum + node.right.val]);
+//   }
+//   return false;
+// };
+
+// DFS Top-Down
+var hasPathSum = function (root, targetSum) {
   if (!root) return false;
-  const q = [[root, root.val]];
-  while (q.length) {
-    const [node, sum] = q.shift();
-    if (!node.left && !node.right && sum === targetSum) return true;
-    if (node.left) q.push([node.left, sum + node.left.val]);
-    if (node.right) q.push([node.right, sum + node.right.val]);
-  }
-  return false;
+  if (!root.left && !root.right) return root.val === targetSum;
+
+  const next = targetSum - root.val;
+  return hasPathSum(root.left, next) || hasPathSum(root.right, next);
 };
